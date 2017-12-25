71f15186f46902676584a3dd1555fea2af1d7561
Harshad Sharma <harshad@sharma.io>
2017-12-25 10:14:42 +0530

    Add barebones functionality with a single index of all files in the hoisted directory and its subdirectories.

--- src/hoistd.nim
+++ src/hoistd.nim
@@ -7,7 +7,7 @@ Usage:
 Options:
   -h --help      Show this screen.
   --version      Show version.
-  --path=<path>  Hoist directory at path [default: . ].
+  --path=<path>  Hoist directory at path [default: ./].
   --bind=<bind>  Bind to IP address [default: 127.0.0.1].
   --port=<port>  Bind to port [default: 8443].
 """
@@ -16,37 +16,41 @@ import strutils
 import docopt
 import terminal
 import asyncdispatch
-import asyncnet
 import strutils
 import jester
 import htmlgen
+import os
+import re
 import cgi
-import logging
 
 
 let args = docopt(doc, version = "0.1.0")
+let static_path = $args["--path"]
+let port = Port(parseInt($args["--port"]))
+let endpoint = $args["--bind"]
 
-
-proc error(msg: string) =
-  styledWriteLine(stderr, fgRed, "ERROR: ", resetStyle, msg)
 
 proc info(msg: string) =
   styledWriteLine(stdout, fgBlue, "INFO: ", resetStyle, msg)
 
 
 settings:
-  port = Port(parseInt($args["--port"]))
-  bind_addr = $args["--bind"]
+  port = port
+  bind_addr = endpoint
 
 
 routes:
   get "/":
-    resp "Hello, world."
+    setStaticDir(request, static_path)
+    var file_list = ""
+    var file_path = ""
+    for file in walkDirRec static_path:
+        file_path = file[2 .. ^1]  # Remove "./" prefix from path.
+        file_list.add li(a(href=encode_url(file_path), file_path))
+    let html = html(head(`title`("hoistd")), body(h3("hoistd"), ul(file_list)))
+    resp ul(`class`="file-list", html)
 
 
-proc main() =
-  info "path: " & $args["--path"]
-  info "hoisted at: https://" & $args["--bind"] & ":" & $args["--port"]
-  runForever()
-
-main()
+info "path: " & $args["--path"]
+info "hoisted at: https://" & $args["--bind"] & ":" & $args["--port"]
+runForever()
